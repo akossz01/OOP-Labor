12b303ce419c10745799838514f13cb35eff0ccc
Akos Szabo <91012723+akossz01@users.noreply.github.com>
2022-02-17 14:08:32 +0200

    OOP 2022

---
+++ labor-01/src/oop/labor01/Kettes.java
@@ -0,0 +1,10 @@
+public class Kettes{
+	public static void main(String args[]){
+		String nev = "Szabo Akos";
+		for( int i=0; i<nev.length(); ++i){
+			System.out.print(nev.charAt(i));
+		}
+		System.out.println("");
+	}
+	
+}
