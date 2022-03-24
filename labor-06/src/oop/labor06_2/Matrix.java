412c25b738df64aec1629f425f797c0e8f77ca12
Akos Szabo <91012723+akossz01@users.noreply.github.com>
2022-03-24 14:06:08 +0200

    6. Labor

--- labor-06/src/oop/labor06_2/Matrix.java
+++ labor-06/src/oop/labor06_2/Matrix.java
@@ -22,8 +22,59 @@ public class Matrix {
         }
     }
 
-    public Matrix(Matrix m){
+    public Matrix(Matrix m) {
         this(m.data);
+    }
+
+    public void fillRandom(double a, double b){
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < columns; ++c) {
+                data[r][c] = Math.random()*(b-a)+a;
+            }
+        }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void printMatrix() {
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < columns; ++c) {
+                System.out.print(this.data[r][c] + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public Matrix add(Matrix m1, Matrix m2) {
+        if (m1.rows != m2.rows || m1.columns != m2.columns) {
+            return null;
+        } else {
+            Matrix m3 = new Matrix(m1.rows, m1.columns);
+            for (int r = 0; r < rows; ++r) {
+                for (int c = 0; c < columns; ++c) {
+                    m3.data[r][c] = m1.data[r][c] + m2.data[r][c];
+                }
+            }
+            return m3;
+        }
+    }
+
+    public Matrix transpose(Matrix m1) {
+        Matrix m2 = new Matrix(m1.rows, m1.columns);
+
+        for (int r = 0; r < m1.rows; ++r) {
+            for (int c = 0; c < m1.columns; ++c) {
+                m2.data[r][c] = m1.data[c][r];
+            }
+        }
+
+        return m2;
     }
 
     //deep copy olyan masolat ami elvan kulonitve - eloszor felveszi ugyanazokat az ertekeket, de utana fuggetlenek
