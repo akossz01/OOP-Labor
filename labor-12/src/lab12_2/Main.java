99442915dc3e5cb3040ae8d73e337672f9ad3ed5
Akos Szabo <91012723+akossz01@users.noreply.github.com>
2022-05-12 13:55:08 +0300

    12. Labor

--- labor-12/src/lab12_2/Main.java
+++ labor-12/src/lab12_2/Main.java
@@ -3,6 +3,7 @@ package lab12_2;
 public class Main {
     public static void main(String[] args) {
         Storage s1 = new Storage("data1000.txt");
+
         s1.update("update1000.txt");
     }
 }
--- labor-12/src/lab12_2/Storage.java
+++ labor-12/src/lab12_2/Storage.java
@@ -3,10 +3,13 @@ package lab12_2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Storage {
-    ArrayList<Product> products = new ArrayList<>();
+    //ArrayList<Product> products = new ArrayList<>();
+    private Map<Integer, Product> products = new HashMap<>();
 
     public Storage(String fileName){
         try (Scanner scanner = new Scanner(new File(fileName))) {
@@ -20,7 +23,7 @@ public class Storage {
                 String name = items[1].trim();
                 int amount =  Integer.parseInt(items[2].trim());
                 int price =  Integer.parseInt(items[3].trim());
-                products.add(new Product(identifier, name, amount, price));
+                products.put(identifier, new Product(identifier, name, amount, price));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -38,14 +41,19 @@ public class Storage {
                 String[] items = line.split(" ");
                 int identifier =  Integer.parseInt(items[0].trim());
                 int newAmount =  Integer.parseInt(items[1].trim());
-                //----Slow--------
-                for (Product p:products){
-                    if (p.getIdentifier() == identifier){
-                        p.increaseAmount(newAmount);
-                        succesfulUpdates++;
+
+                /*for (Map.Entry<Integer, Product> entry: products.entrySet()){
+                    if(entry.getKey() == identifier){
+                        entry.getValue().increaseAmount(newAmount);
                     }
+                }*/ // Az alabbi megoldas sokkal jobb es gyorsabb
+
+                //letrehoz egy uj referenciat, ami a megfelelo elemre mutat a Map-bol
+                Product p = products.get(identifier); //olyan mintha egy pointer lenne
+                if(p != null){
+                    p.increaseAmount(newAmount);
+                    succesfulUpdates++;
                 }
-                //----------------
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -53,7 +61,7 @@ public class Storage {
         System.out.println("Number of succesfully updated files: " + succesfulUpdates);
     }
 
-    public ArrayList<Product> printStorage(){
+    public Map<Integer, Product> printStorage(){
         return products;
     }
 }
